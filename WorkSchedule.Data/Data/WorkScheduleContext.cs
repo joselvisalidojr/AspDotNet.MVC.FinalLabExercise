fa91d50ca685a0f5ef2adccf4527cf9e42219cd6
joselvisalidojr <joselvisalidojr@gmail.com>
2022-05-06 11:55:35 +0800

    Add project files.

---
+++ WorkSchedule.Data/Data/WorkScheduleContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSchedule.Data.Entities;
+
+namespace WorkSchedule.Data
+{
+    public class WorkScheduleContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public WorkScheduleContext(DbContextOptions<WorkScheduleContext> options) : base(options)
+        {
+        }
+
+        public WorkScheduleContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeSkill> EmployeeSkills { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<PlacementContract> PlacementContracts { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(this.connectionString);
+            }
+        }
+    }
+}
